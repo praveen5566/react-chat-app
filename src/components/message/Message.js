3ac25a51e993b6d183081ea93c9e94fca01a89d1
Praveen Ningappa <praveen5566@users.noreply.github.com>
2019-10-10 02:30:20 -0700

    add auto scroll view

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Message.css';
 import { USER_NAME } from '../../constants';
 
 export const Message = (props) => {
     const { id, message } = props;
     const [isSelf, setIsSelf] = useState(true);
+    const messageScroll = useRef(null);
 
     useEffect(() => {
         setIsSelf(localStorage.getItem(USER_NAME) === message.name);
+        let node = messageScroll.current;
+        node.scrollIntoView({ block: 'start', behavior: 'smooth', inline: 'nearest' });
     }, []);
 
     return (
         <li key={id}>
             {isSelf ?
-                <div className="message-wrapper-self">
+                <div ref={messageScroll} className="message-wrapper-self">
                     <div className="message-text-self">{message.message}</div>
                 </div> :
-                <div className="message-wrapper">
+                <div ref={messageScroll} className="message-wrapper">
                     <div className="message-text">{message.message}</div>
                     <div className="message-name">{message.name}</div>
                 </div>}
