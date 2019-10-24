cef4ef1193c4b7d45a7cda8afb41c5272491921e
Praveen Ningappa <praveen5566@users.noreply.github.com>
2019-10-23 22:54:26 -0700

    add procfile for heroku

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express    = require('express')
 var app        = express()
 var bodyParser = require('body-parser')
 var shortid = require('shortid')
+var cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors());
 
 var port = process.env.PORT || 8080
 
